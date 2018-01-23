6af7a251fa0637c21df08094f1edb45b17722fba
Lee Gyeong Taek <skkutak@gmail.com>
2018-01-23 17:16:00 +0900

    Update qnetwork.R

--- qnetwork.R
+++ qnetwork.R
@@ -66,13 +66,12 @@ return_reward<-function(state,current_state){
   re_index<-which(state==1)
   
   if(  re_index==100){
-    reward<- 100# episode end
+    reward<- 5# episode end
     done<-T
   }
-  else if(re_index==12 | re_index==20|re_index==31|re_index==38|
-          re_index==42|re_index==44|re_index==45    |
+  else if(re_index==12 |re_index==42|re_index==44|re_index==45    |
           re_index==68|re_index==72|re_index==80){
-    reward<- -5 
+    reward<- -2
     done<-F
   }else{
     reward <- -1
@@ -81,7 +80,11 @@ return_reward<-function(state,current_state){
   if(re_index==which(current_state==1)){
     reward<-reward*2
   }
-
+  xx<-ceiling(re_index/ 10) ## row
+  yy<-re_index %% 10  ## col
+  yy<-ifelse(yy ==0,10,yy)
+  reward_weight<-sqrt(162)-sqrt((yy-10)^2+(xx-10)^2) #weigthed reward by distance from current state to goal
+  reward<-reward+reward_weight*0.05
   c(reward,done)
   
 }
@@ -89,9 +92,9 @@ action<-c("left","right","down","up")
 ## 10 x 10  frozen lake problem
 # S : start, F : Frozen, H : Hole, G : Goal 
 # SFFFF|FFFFF
-# FHFFF|FFFFH
+# FHFFF|FFFFF
 # FFFFF|FFFFF
-# HFFFF|FFHFF
+# FFFFF|FFFFF
 # FHFHH|FFFFF
 # FFFFF|FFFFF
 # FFFFF|FFHFF
@@ -114,7 +117,7 @@ action<-c("left","right","down","up")
   hidden_dropout = 0
   visible_dropout = 0
   numepochs = 10
-  learningrate<-0.1
+  learningrate<-0.01
   
   vW <- list()
   vB <- list()
@@ -152,9 +155,7 @@ q_table<-list()
 for(i in 1:10000){
   total_r<-0
   episode_done<-0
-  da<-diag(100)
-  diag(da)<-init_data
-  qn1<-nn.ff2(qn1,da)
+  qn1<-nn.ff2(qn1,t(init_data))
   step<-1
   action_list<-NULL
   st<-c(1,1)
@@ -163,15 +164,14 @@ for(i in 1:10000){
   while(episode_done==0){
     
     if(step >1){
-      da<-diag(100)
-      diag(da)<-next_state
-      qn1<-nn.ff2(qn1,da)
-      action_index<-which.max(qn1$post[[length(size)]][next_state==1,])
+ 
+      qn1<-nn.ff2(qn1,t(next_state))
+      action_index<-which.max(qn1$post[[length(size)]])
       current_state<-next_state
       
     }else{
       current_state<-init_data
-      action_index<-which.max(qn1$post[[length(size)]][1,])
+      action_index<-which.max(qn1$post[[length(size)]])
     }
     
    
@@ -187,27 +187,23 @@ for(i in 1:10000){
     action_list<-c(action_list,next_action)
     st<-move(st,next_action)
     state_index<-stm[st[1],st[2]]
-    if(state_index==100){
-      # break
-    }
+  
     next_state<-next_where(state_index)
     
     #### target value  
-    da<-diag(100)
-    diag(da)<-next_state
-    qn2<-nn.ff2(qn1,da)
+  
+    qn2<-nn.ff2(qn1,t(next_state))
     re_ep<-return_reward(next_state,current_state) ## get a reward and Whether the episode ends for action(next state)
     qv<-qn2$post[[length(qn2$size)]] 
     
     if(state_index==100){ ## if episonde done
       
-      re<-    rep(0,100)
-      qv[current_state==1,action_index] <- re_ep[1]
+      qv[action_index] <- re_ep[1]
       true_y<-qv
       
     }else{
-      re<- rep(0,100)
-      qv[current_state==1,action_index] <-  re_ep[1]+ dis_f * max(qv[current_state==1])
+  
+      qv[action_index] <-  re_ep[1]+ dis_f * max(qv)
       true_y<- qv
     }
     qn1$e<-   true_y-qn1$post[[length(size)]]
@@ -232,7 +228,7 @@ for(i in 1:10000){
       step_list<-c(step_list,step)
       final_action_list[[i]]<-action_list
       reward_list<-c(reward_list,total_r)
-      cat("\final location")
+      cat("\n final location")
       print(coord(next_state))
       if(i %%10==0){
       ad<-apply(nn.ff2(qn1,diag(100))$post[[3]],1,which.max);ad
@@ -250,7 +246,7 @@ for(i in 1:10000){
     
     ### max step
     if(step == 100){
-      cat("\final location")
+      cat("\n final location")
       print(coord(next_state))
       step_list<-c(step_list,step)
       final_action_list[[i]]<-action_list
@@ -270,8 +266,6 @@ for(i in 1:10000){
   
 }
 
-
-  
-  ts.plot(step_list,main="step")
-  ts.plot(reward_list,main="Qnetwork-reward")
+ts.plot(step_list,main="step")
+ts.plot(reward_list,main="Qnetwork-reward")
   
